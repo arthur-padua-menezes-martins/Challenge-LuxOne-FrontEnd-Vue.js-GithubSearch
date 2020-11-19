4fd8f4e79ac205dd173edc317457149c884d1fcb
arthur-padua-menezes-martins <arthur.software.developer@gmail.com>
2020-11-19 16:48:00 -0300

    feat: should call axios with correct url

--- src/infra/http/client/axios/axios-http-client.ts
+++ src/infra/http/client/axios/axios-http-client.ts
@@ -1,9 +1,12 @@
+import axios from 'axios'
 import {
   IHttpClientGet, IHttpClientGetParams
 } from './axios-http-client-protocols' 
 
 export class AxiosHttpClient {
   async get (params: IHttpClientGetParams): Promise<void> {
+    const { url } = params
 
+    await axios.get(url)
   }
 }
