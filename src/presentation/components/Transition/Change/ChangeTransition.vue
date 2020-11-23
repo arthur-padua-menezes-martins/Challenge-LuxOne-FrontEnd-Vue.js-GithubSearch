9ecd888a454db23e66b123e802aa0e82ed9ca42e
arthur-padua-menezes-martins <arthur.software.developer@gmail.com>
2020-11-22 22:50:47 -0300

    feat: create ChangeTransition component to apply before and after the request

---
+++ src/presentation/components/Transition/Change/ChangeTransition.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="mov_relative">
+    <div id="component-change-transition" class="trs__two-second">
+      <svg class="spinner" viewBox="0 0 50 50"><circle class="path" cx="25" cy="25" r="20" fill="none" stroke-width="5"></circle></svg>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from '@/main/config/app'
+
+const ComponentChangeTransition = Vue.component('ComponentChangeTransition', {
+  el: '#component-change-transition',
+
+  components: {
+
+  },
+
+  methods: {
+
+  },
+
+  data () {
+
+  },
+
+  mounted () {
+
+  }
+})
+
+export default ComponentChangeTransition
+</script>
+
+<style src="./style.sass" lang="sass"></style>
