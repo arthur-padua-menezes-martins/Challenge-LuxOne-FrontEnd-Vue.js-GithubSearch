5e556e9194e081d6a980c92332bc61fb6fadf7a9
arthur-padua-menezes-martins <arthur.software.developer@gmail.com>
2020-11-23 21:58:30 -0300

    chore: update configs

---
+++ server.js
@@ -0,0 +1,12 @@
+const path = require('path')
+
+const express = require('express')
+const serveStatic = require('serve-static')
+const app = express()
+
+app.use(serveStatic(path.join(__dirname, '/build')))
+
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+  console.log(`server started ${port}`)
+})
