cf57b123b1b8a705155bf3a57e5abf50053d90a0
arthur-padua-menezes-martins <arthur.software.developer@gmail.com>
2020-11-23 22:18:33 -0300

    refactor: paths refactor

--- src/main/config/global.ts
+++ src/main/config/global.ts
@@ -1,9 +1,9 @@
-import Vue from '@/main/config/app'
-import PageSearch from '@/presentation/pages/Search/Search.vue'
-import PagePerfil from '@/presentation/pages/Perfil/Perfil.vue'
-import ComponentInitialTransition from '@/presentation/components/Transition/Initial/InitialTransition.vue'
-import ComponentChangeTransition from '@/presentation/components/Transition/Change/ChangeTransition.vue'
-import ComponentSearchBar from '@/presentation/components/SearchBar/SearchBar.vue'
+import Vue from '../../main/config/app'
+import PageSearch from '../../presentation/pages/Search/Search.vue'
+import PagePerfil from '../../presentation/pages/Perfil/Perfil.vue'
+import ComponentInitialTransition from '../../presentation/components/Transition/Initial/InitialTransition.vue'
+import ComponentChangeTransition from '../../presentation/components/Transition/Change/ChangeTransition.vue'
+import ComponentSearchBar from '../../presentation/components/SearchBar/SearchBar.vue'
 
 interface IDefinitionsType {
   [field: string]: any
